1c275403929a90d886b68913e1328d875642f1c0
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2022-03-31 16:55:23 +0200

    added option to enable/disable pausing the game when it's not focused

--- elden_pause/include/misc.h
+++ elden_pause/include/misc.h
@@ -17,8 +17,10 @@ namespace config
     const std::string KEY_KEYBOARD_BUTTON{ "KeyboardButton" };
     const std::string KEY_CONTROLLER_BUTTON{ "ControllerButton" };
     const std::string KEY_CONTROLLER_BUTTON2{ "ControllerButton2" };
+    const std::string KEY_PAUSE_WHEN_UNFOCUSED{ "PauseWhenUnfocused" };
     std::string OPTION_CONTROLLER_ENABLED{ "true" }; // Disable or enable the usage of controller code. Enabled by default
     int32_t OPTION_KEYBOARD_BUTTON{ 0x50 }; // "P" button by default
     int32_t OPTION_CONTROLLER_BUTTON{ XINPUT_GAMEPAD_START }; // "Start" button by default
     int32_t OPTION_CONTROLLER_BUTTON2{ XINPUT_GAMEPAD_NONE }; // Disabled by default
+    bool OPTION_PAUSE_WHEN_UNFOCUSED{ false };
 }
